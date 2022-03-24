a38368d68bbe44ac31b9f740d020b985a0418a2a
Denis Istrate <92303480+denistr1997@users.noreply.github.com>
2022-03-25 00:13:15 +0200

    Update fetchWeather
    "http://" missing

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-let weather = { fetchWeather: function(city) { fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=c6aab8b0e9f68978dbc15a08de481a14").then((response) => response.json()).then((data) => console.log(data)); }, };
+let weather = {
+    // apiKey: "c6aab8b0e9f68978dbc15a08de481a14",
+    fetchWeather: function(city) { fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=c6aab8b0e9f68978dbc15a08de481a14").then((response) => response.json()).then((data) => console.log(data)); },
+    displayWeather: function(data) {
+
+    }
+};
