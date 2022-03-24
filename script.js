64de3d8ad74387e6d5a06595297b93cb629f09f8
Denis Istrate <92303480+denistr1997@users.noreply.github.com>
2022-03-24 23:53:19 +0200

    Background update + js update

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-let weather = {
-    "apiKey": "c6aab8b0e9f68978dbc15a08de481a14"
-    fetchWeather: function() {
-        fetch(
-                "https://api.openweathermap.org/data/2.5/weather?q=  +
-                city +
-                "&units=metric&appid=" +
-                appKey;
-            )
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    },
-};
+let weather = { fetchWeather: function(city) { fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=c6aab8b0e9f68978dbc15a08de481a14").then((response) => response.json()).then((data) => console.log(data)); }, };
