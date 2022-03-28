cbbe7c5c858d859080b05e18d7d34748477e4e4a
Denis Istrate <92303480+denistr1997@users.noreply.github.com>
2022-03-28 12:46:01 +0300

    Weather Display

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 let weather = {
-    // apiKey: "c6aab8b0e9f68978dbc15a08de481a14",
-    fetchWeather: function(city) { fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=c6aab8b0e9f68978dbc15a08de481a14").then((response) => response.json()).then((data) => console.log(data)); },
+    apiKey: "c6aab8b0e9f68978dbc15a08de481a14",
+    fetchWeather: function(city) {
+        fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=imperial&appid=" +
+                this.apiKey
+            )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
     displayWeather: function(data) {
-
-    }
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " mph";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
